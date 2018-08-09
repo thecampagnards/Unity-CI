b94b8361cf143b43ee032bbe9cb86db4c8a097b4
Konstantin Sidorenko <konstantin.sidorenko@orange.fr>
2018-08-09 22:59:04 +0200

    Debug

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -2,10 +2,14 @@
 
 echo "Attempting to test"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
   -runTests \
   -projectPath "$(pwd)" \
   -testResults "$(pwd)"/results.xml \
   -testPlatform editmode
+  -quit
 
 echo 'Results from tests'
 cat "$(pwd)"/results.xml
